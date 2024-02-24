704ca2fb21e8c96b94452a3bf9ea1e1006853796
Quan Vo <tranminhquanvo@gmail.com>
2024-02-24 14:32:16 +0700

    fix: center items in the home page

--- apps/participant/src/app/page.tsx
+++ apps/participant/src/app/page.tsx
@@ -11,28 +11,30 @@ const Page = async () => {
   return (
     <div className="flex flex-col">
       <PageHeader title="Trang chủ" />
-      {task ? (
-        <div className="flex flex-col items-center gap-4">
-          <Image
-            alt="Task description"
-            height={856}
-            src="/images/poster.jpeg"
-            width={1024}
-          />
-          <Link
-            className="text-blue-500 hover:text-blue-700 hover:underline pb-4"
-            href={task.url}
-            target="_blank"
-          >
-            NHIỆM VỤ TUẦN {task.week}
-          </Link>
-        </div>
-      ) : (
-        <>
-          <ListAlt className="!text-[256px]" />
-          <h1>Chưa có nhiệm vụ.</h1>
-        </>
-      )}
+      <div className="flex flex-col items-center gap-4">
+        {task ? (
+          <>
+            <Image
+              alt="Task description"
+              height={856}
+              src="/images/poster.jpeg"
+              width={1024}
+            />
+            <Link
+              className="text-blue-500 hover:text-blue-700 hover:underline pb-4"
+              href={task.url}
+              target="_blank"
+            >
+              NHIỆM VỤ TUẦN {task.week}
+            </Link>
+          </>
+        ) : (
+          <>
+            <ListAlt className="!text-[256px]" />
+            <h1>Chưa có nhiệm vụ.</h1>
+          </>
+        )}
+      </div>
     </div>
   );
 };
